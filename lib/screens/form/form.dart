ee951efca4898e3e3771c34a8f0f1893dc88e917
unknown <nad279444>
2021-12-15 08:34:18 -0800

    created form functionality

--- lib/screens/form/form.dart
+++ lib/screens/form/form.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class FormInput extends StatelessWidget {
+class FormInput extends StatefulWidget {
   const FormInput({Key? key}) : super(key: key);
+
+  @override
+  State<FormInput> createState() => _FormInputState();
+}
+
+class _FormInputState extends State<FormInput> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  final TextEditingController _titleController = TextEditingController();
+
+  final TextEditingController _descriptionController = TextEditingController();
+
+  final TextEditingController _dateController = TextEditingController();
+
+  final TextEditingController _timeController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _titleController.clear();
+    _descriptionController.clear();
+    _dateController.clear();
+    _timeController.clear();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +43,17 @@ class FormInput extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.only(top: 50),
         child: Form(
+          key: _formKey,
           child: ListView(
             children: [
               ListTile(
                   title: TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'title cannot be left empty';
+                  }
+                },
+                controller: _titleController,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.redAccent)),
@@ -34,6 +68,9 @@ class FormInput extends StatelessWidget {
               ),
               ListTile(
                   title: TextFormField(
+                controller: _descriptionController,
+                validator: (value) =>
+                    value!.isEmpty ? 'Enter a valid description' : null,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.redAccent)),
@@ -52,6 +89,26 @@ class FormInput extends StatelessWidget {
                   Expanded(
                     child: ListTile(
                         title: TextFormField(
+                      onTap: () {
+                        showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime.now(),
+                                lastDate: DateTime.now()
+                                    .add(const Duration(days: 365)))
+                            .then((value) {
+                          setState(() {
+                            _dateController.text =
+                                DateFormat.yMMMEd().format(value!);
+                          });
+                        });
+                      },
+                      controller: _dateController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter a date';
+                        }
+                      },
                       decoration: const InputDecoration(
                           border: OutlineInputBorder(
                               borderSide: BorderSide(color: Colors.redAccent)),
@@ -65,6 +122,21 @@ class FormInput extends StatelessWidget {
                   Expanded(
                     child: ListTile(
                         title: TextFormField(
+                      onTap: () {
+                        showTimePicker(
+                                context: context, initialTime: TimeOfDay.now())
+                            .then((value) {
+                          setState(() {
+                            _timeController.text = value!.format(context);
+                          });
+                        });
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter a time';
+                        }
+                      },
+                      controller: _timeController,
                       decoration: const InputDecoration(
                           border: OutlineInputBorder(
                               borderSide: BorderSide(color: Colors.redAccent)),
@@ -85,7 +157,20 @@ class FormInput extends StatelessWidget {
                     style: TextButton.styleFrom(
                         backgroundColor: Colors.redAccent,
                         padding: const EdgeInsets.all(25)),
-                    onPressed: () {},
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        String dateTime =
+                            '${_dateController.text} ${_timeController.text} ';
+                        print(_titleController.text);
+                        print(_descriptionController.text);
+                        print(dateTime);
+
+                        _titleController.clear();
+                        _descriptionController.clear();
+                        _dateController.clear();
+                        _timeController.clear();
+                      }
+                    },
                     child: const Text(
                       'Save',
                       style: TextStyle(
